81dbd665c72281b3733631eef5a4408952a82f29
Muhammad Ibraheem <160319187+MuhammadIbrahim557@users.noreply.github.com>
2024-04-04 08:49:42 -0700

    Push this basic To do list to github

---
+++ main.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let toDoList = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "toDo",
+            type: "input",
+            message: "What you want to do iwa toDos?",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more in toDos?",
+            default: "false"
+        }
+    ]);
+    toDoList.push(addTask.toDo);
+    condition = addTask.addMore;
+    console.log(toDoList);
+}
